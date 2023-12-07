4f8f73e84b023ed43d1278623c538559b5df6afa
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-12-07 11:25:52 -0300

    changedColor value

--- Weather App/Color+Extensions.swift
+++ Weather App/Color+Extensions.swift
@@ -14,6 +14,6 @@ extension UIColor {
     
     static let primaryColor = UIColor(named: "primaryColor")
     
-    static let lightGray = UIColor(named: "softgray")
+    static let lightGray = UIColor(named: "softGray")
     
 }
