90054cbed610a8db223336efbc58f98f372b4a81
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-12-07 11:25:29 -0300

    UIColor extension

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Guilherme Fonseca on 07/12/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let contrastColor = UIColor(named: "contrastColor")
+    
+    static let primaryColor = UIColor(named: "primaryColor")
+    
+    static let lightGray = UIColor(named: "softgray")
+    
+}
