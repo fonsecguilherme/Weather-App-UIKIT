127c3446a9cc01d01884736fdff6ac8e6502e186
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-12-09 17:05:54 -0300

    Created UIView extension

--- Weather App/UIView+Extensions.swift
+++ Weather App/UIView+Extensions.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView{
+    func setConstraintsToParent(_ parent: UIView) {
+        
+        /*
+         uso o self para estabelecer em relação a view que estou chamando
+         e o parente a view que eu estou passando
+         */
+        
+        NSLayoutConstraint.activate([
+        
+            self.topAnchor.constraint(equalTo: parent.topAnchor),
+            self.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: parent.trailingAnchor),
+            self.bottomAnchor.constraint(equalTo: parent.bottomAnchor)
+            
+        ])
+    }
+}
